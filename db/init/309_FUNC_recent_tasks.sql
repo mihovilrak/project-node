2dc27345ca5d008210f65951f1601e52bf3c4c9e
mihovilrak-gp <mihovil.rak@geoprojekt-zg.hr>
2026-02-02 08:47:44 +0100

    fixed recent tasks rendering

--- db/init/309_FUNC_recent_tasks.sql
+++ db/init/309_FUNC_recent_tasks.sql
@@ -16,18 +16,23 @@ begin
     return query
         select t.id,
                t.name,
-               t.project_name,
-               t.holder_name,
+               po.name as project_name,
+               h.name as holder_name,
                t.estimated_time,
                t.start_date,
                t.due_date,
-               t.status_name,
-               t.priority_name,
-               t.created_by_name,
-               t.priority_color
-        from v_tasks t
-        where t.assignee_id = user_id
-        and t.status_name not in ('Deleted', 'Cancelled', 'Done')
+               ts.name as status_name,
+               pi.name as priority_name,
+               c.name as created_by_name,
+               pi.color as priority_color
+        from tasks t
+        left join projects po on po.id = t.project_id
+        left join users h on h.id = t.holder_id
+        left join users c on c.id = t.created_by
+        left join task_statuses ts on ts.id = t.status_id
+        left join priorities pi on pi.id = t.priority_id
+        where t.assignee_id = recent_tasks.user_id
+        and ts.name not in ('Deleted', 'Cancelled', 'Done')
         order by t.created_on desc
         limit 10;
 end;
