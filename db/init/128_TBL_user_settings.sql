c065e682f683fd525dfc66a14ecf099ce3a06ad2
mihovilrak-gp <mihovil.rak@geoprojekt-zg.hr>
2026-02-12 13:22:24 +0100

    fixed unique constraint

--- db/init/128_TBL_user_settings.sql
+++ db/init/128_TBL_user_settings.sql
@@ -1,11 +1,10 @@
 create table if not exists user_settings (
     id int primary key generated always as identity not null,
-    user_id int references users(id) not null,
+    user_id int unique references users(id) not null,
     theme varchar(255) not null default 'light',
     language varchar(255) not null default 'en',
     notifications_enabled boolean not null default true,
     email_notifications_enabled boolean not null default true,
     created_on timestamp not null default current_timestamp,
-    updated_on timestamp not null default current_timestamp,
-    constraint user_settings_unique unique (user_id)
+    updated_on timestamp not null default current_timestamp
 );
